c0f20791920afb923edf6ca6c7d9541f617ea980
nadid <nadid@DESKTOP-I95TG51>
2021-02-27 02:44:47 +0300

    WebAPI

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -32,7 +32,7 @@ namespace Business.Concrete
         }
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==15)
+            if (DateTime.Now.Hour==05)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
